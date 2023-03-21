aed83999095f92b15109f4a098be747eb4e444fe
cesarhenrq <cesaarhvb@gmail.com>
2023-03-20 22:26:08 -0300

    feat: create theme

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#017849',
+    },
+    secondary: {
+      main: '#FFFFFF',
+    },
+  },
+});
+
+export default theme;
