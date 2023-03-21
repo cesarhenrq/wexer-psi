74d682c9ab84fa2b185bb45dbea1163ff3e831a8
cesarhenrq <cesaarhvb@gmail.com>
2023-03-20 22:35:04 -0300

    alert: will be modificaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { ThemeProvider } from '@mui/material/styles';
+import { Normalize } from 'styled-normalize';
+import BaseLayout from './components/base-layout';
+import theme from './theme';
+
 function App() {
-  return <div className="App">It works!</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Normalize />
+        <BaseLayout />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
