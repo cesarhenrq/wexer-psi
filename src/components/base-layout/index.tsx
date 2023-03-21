247a6825f2e52db3c5b60ac3ceb7af90c3631b7a
cesarhenrq <cesaarhvb@gmail.com>
2023-03-20 22:33:56 -0300

    feat: implement base layout

---
+++ src/components/base-layout/index.tsx
@@ -0,0 +1,13 @@
+import Header from '../header';
+import NavMenu from '../nav-menu';
+
+const BaseLayout = () => {
+  return (
+    <div>
+      <Header />
+      <NavMenu />
+    </div>
+  );
+};
+
+export default BaseLayout;
