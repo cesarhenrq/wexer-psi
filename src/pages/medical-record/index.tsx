e2dce0c373542323d086f8dc80c07b4bbd23426a
cesarhenrq <cesaarhvb@gmail.com>
2023-03-28 03:35:33 -0300

    feat: add new funcionalities

--- src/pages/medical-record/index.tsx
+++ src/pages/medical-record/index.tsx
@@ -1,5 +1,26 @@
+import DataPatientCard from '../../components/data-patient-card';
+import GenericCard from '../../components/generic-card';
+import ModalToolBar from '../../components/modal-tool-bar';
+import * as S from './styles';
+
 const MedicalRecord = () => {
-  return <div>It Works!</div>;
+  const content =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit blanditiis nobis, quas quibusdam porro commodi consequuntur libero velit necessitatibus optio distinctio corrupti totam, non fugiat quaerat magni enim sit numquam';
+  return (
+    <div>
+      <S.Grid>
+        <S.CardContainer>
+          <DataPatientCard />
+          <GenericCard title="Demandas e objetivos" content={content} />
+          <GenericCard title="Anotações pessoais" content={content} />
+        </S.CardContainer>
+        <S.Container>
+          <ModalToolBar />
+        </S.Container>
+        <S.Content>Conteúdo</S.Content>
+      </S.Grid>
+    </div>
+  );
 };
 
 export default MedicalRecord;
