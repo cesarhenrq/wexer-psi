2abeda94af99e7b3ba09ddaca8e191436e033da6
cesarhenrq <cesaarhvb@gmail.com>
2023-03-28 23:24:56 -0300

    refactor: width, height and color will be a prop

--- src/components/clipboard-icon/index.tsx
+++ src/components/clipboard-icon/index.tsx
@@ -1,14 +1,20 @@
-const ClipboardIcon = () => {
+type ClipboardIconProps = {
+  color: string;
+  width: string;
+  height: string;
+};
+
+const ClipboardIcon = ({ color, width, height }: ClipboardIconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width={width}
+      height={height}
       fill="none"
-      viewBox="0 0 24 24"
+      viewBox={`0 0 ${width} ${height}`}
     >
       <path
-        fill="#fff"
+        fill={color}
         d="M17 2h3a1 1 0 011 1v18a1 1 0 01-1 1H4a1 1 0 01-1-1V3a1 1 0 011-1h3V0h2v2h6V0h2v2zM7 8v2h10V8H7zm0 4v2h10v-2H7z"
       ></path>
     </svg>
