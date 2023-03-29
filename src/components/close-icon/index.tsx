b511efd7d7ba7ae7759164edca55424544c1318c
cesarhenrq <cesaarhvb@gmail.com>
2023-03-29 04:31:31 -0300

    implement: close icon component

---
+++ src/components/close-icon/index.tsx
@@ -0,0 +1,25 @@
+const CloseIcon = () => 
+ (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <g clipPath="url(#clip0_4022_25039)">
+        <path
+          fill="#09121F"
+          d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-11.414L9.172 7.757 7.757 9.172 10.586 12l-2.829 2.828 1.415 1.415L12 13.414l2.828 2.829 1.415-1.415L13.414 12l2.829-2.828-1.415-1.415L12 10.586z"
+        ></path>
+      </g>
+      <defs>
+        <clipPath id="clip0_4022_25039">
+          <path fill="#fff" d="M0 0H24V24H0z"></path>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+
+
+export default CloseIcon;
