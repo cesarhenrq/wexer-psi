539bd280e0de51e2d20c0b2843b779fc02db09d7
cesarhenrq <cesaarhvb@gmail.com>
2023-03-28 23:22:21 -0300

    feat: implement clip icon component

---
+++ src/components/clip-icon/index.tsx
@@ -0,0 +1,25 @@
+const ClipIcon = () => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        fill="none"
+        viewBox="0 0 32 32"
+      >
+        <g clipPath="url(#clip0_4022_24912)">
+          <path
+            fill="#9D28AC"
+            d="M19.77 10.343l-7.54 7.542a1.333 1.333 0 101.885 1.886l7.542-7.542A4 4 0 0016 6.572l-7.543 7.543a6.667 6.667 0 109.428 9.426L25.428 16l1.885 1.885-7.542 7.543a9.332 9.332 0 01-13.2 0 9.333 9.333 0 010-13.2l7.544-7.541a6.667 6.667 0 019.426 9.426l-7.54 7.546a4.001 4.001 0 01-5.658-5.66l7.542-7.542 1.886 1.886z"
+          ></path>
+        </g>
+        <defs>
+          <clipPath id="clip0_4022_24912">
+            <path fill="#fff" d="M0 0H32V32H0z"></path>
+          </clipPath>
+        </defs>
+      </svg>
+    );
+  }
+  
+  export default ClipIcon;
