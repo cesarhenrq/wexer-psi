4ceec62bb39c5efb63d2afd64aa76ac9f72420ed
cesarhenrq <cesaarhvb@gmail.com>
2023-03-28 23:27:39 -0300

    test: just testing new features

--- src/pages/medical-record/index.tsx
+++ src/pages/medical-record/index.tsx
@@ -1,9 +1,13 @@
+//import { useState } from 'react';
 import DataPatientCard from '../../components/data-patient-card';
 import GenericCard from '../../components/generic-card';
 import ModalToolBar from '../../components/modal-tool-bar';
+import SessionModal from '../../components/session-modal';
 import * as S from './styles';
 
 const MedicalRecord = () => {
+  /*const [isOpenModal, setIsOpenModal] = useState(false);*/
+
   const content =
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit blanditiis nobis, quas quibusdam porro commodi consequuntur libero velit necessitatibus optio distinctio corrupti totam, non fugiat quaerat magni enim sit numquam';
   return (
@@ -14,10 +18,12 @@ const MedicalRecord = () => {
           <GenericCard title="Demandas e objetivos" content={content} />
           <GenericCard title="Anotações pessoais" content={content} />
         </S.CardContainer>
-        <S.Container>
+        <S.ModalToolBarContainer>
           <ModalToolBar />
-        </S.Container>
-        <S.Content>Conteúdo</S.Content>
+        </S.ModalToolBarContainer>
+        <S.ContentContainer>
+          <SessionModal />
+        </S.ContentContainer>
       </S.Grid>
     </div>
   );
