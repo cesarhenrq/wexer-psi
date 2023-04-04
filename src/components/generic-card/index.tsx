a334daa0dc2d016ed19350bb352bb033afca27ec
cesarhenrq <cesaarhvb@gmail.com>
2023-04-04 04:23:39 -0300

    feat: add interation

--- src/components/generic-card/index.tsx
+++ src/components/generic-card/index.tsx
@@ -1,21 +1,39 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import EditButton from '../edit-button';
+import { Dispatch, SetStateAction } from 'react';
+import { Card, CardContent, Typography, Button } from '@mui/material';
+import EditIcon from '../edit-icon';
 import * as S from './styles';
 
 type GenericCardProps = {
   title: string;
   content: string;
+  modal: ModalsStateKeys;
+  setModalsState: Dispatch<SetStateAction<ModalsStateT>>;
 };
 
-const GenericCard = ({ title, content }: GenericCardProps) => {
+const GenericCard = ({
+  title,
+  content,
+  modal,
+  setModalsState,
+}: GenericCardProps) => {
+  const handleClick = <Keys extends ModalsStateKeys>(key: Keys) => {
+    setModalsState((prevValue) => ({
+      ...prevValue,
+      [key]: true,
+    }));
+  };
   return (
     <Card>
       <CardContent>
         <S.Title>
           <Typography variant="h6">{title}</Typography>
-          <EditButton />
+          <Button
+            variant="outlined"
+            sx={{ width: 32, height: 24, padding: 0 }}
+            onClick={() => handleClick(modal)}
+          >
+            <EditIcon />
+          </Button>
         </S.Title>
         <div>{content}</div>
       </CardContent>
