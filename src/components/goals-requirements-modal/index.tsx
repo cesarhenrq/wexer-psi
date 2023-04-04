5ae98803163f2f59690e58861bfc91437ccd1847
cesarhenrq <cesaarhvb@gmail.com>
2023-04-04 04:18:37 -0300

    feat: implement goals requirements modal

---
+++ src/components/goals-requirements-modal/index.tsx
@@ -0,0 +1,18 @@
+import { useContext } from 'react';
+import { ModalContext } from '../../contexts/ModalContext';
+import ModalBaseLayout from '../modal-base-layout';
+
+const GoalsRequirementsModal = () => {
+  const { modalsState } = useContext(ModalContext);
+  return (
+    <ModalBaseLayout
+      title="Demandas e Objetivos"
+      modalState={modalsState.isGoalsRequirementsModalOpen}
+      modal="isGoalsRequirementsModalOpen"
+    >
+      <p>Modal Objetivos e Demandas</p>
+    </ModalBaseLayout>
+  );
+};
+
+export default GoalsRequirementsModal;
