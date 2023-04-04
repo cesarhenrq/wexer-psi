902edb29c82c065607b60aa0d625162716c25d39
cesarhenrq <cesaarhvb@gmail.com>
2023-04-04 04:26:21 -0300

    feat: add hooks and interactions

--- src/pages/medical-record/index.tsx
+++ src/pages/medical-record/index.tsx
@@ -1,31 +1,59 @@
-//import { useState } from 'react';
+import { useState } from 'react';
+import { ModalContext } from '../../contexts/ModalContext';
 import DataPatientCard from '../../components/data-patient-card';
 import GenericCard from '../../components/generic-card';
+import GoalsRequirementsModal from '../../components/goals-requirements-modal';
 import ModalToolBar from '../../components/modal-tool-bar';
+import PersonalNotesModal from '../../components/personal-notes-modal';
 import SessionModal from '../../components/session-modal';
 import * as S from './styles';
+import AttachmentModal from '../../components/attachment-modal';
+import PertinentFactModal from '../../components/pertinent-fact-modal';
+import PsychologicalAssessmentModal from '../../components/psychological-assessment-modal';
 
 const MedicalRecord = () => {
-  /*const [isOpenModal, setIsOpenModal] = useState(false);*/
+  const [modalsState, setModalsState] = useState<ModalsStateT>({
+    isSessionModalOpen: false,
+    isPertinentFactModalOpen: false,
+    isAttachmentModalOpen: false,
+    isPsychologicalAssessmentModalOpen: false,
+    isGoalsRequirementsModalOpen: false,
+    isPersonalNotesModalOpen: false,
+  });
 
   const content =
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit blanditiis nobis, quas quibusdam porro commodi consequuntur libero velit necessitatibus optio distinctio corrupti totam, non fugiat quaerat magni enim sit numquam';
   return (
-    <div>
+    <ModalContext.Provider value={{ setModalsState, modalsState }}>
       <S.Grid>
         <S.CardContainer>
           <DataPatientCard />
-          <GenericCard title="Demandas e objetivos" content={content} />
-          <GenericCard title="Anotações pessoais" content={content} />
+          <GenericCard
+            title="Demandas e objetivos"
+            content={content}
+            modal="isGoalsRequirementsModalOpen"
+            setModalsState={setModalsState}
+          />
+          <GenericCard
+            title="Anotações pessoais"
+            content={content}
+            modal="isPersonalNotesModalOpen"
+            setModalsState={setModalsState}
+          />
         </S.CardContainer>
         <S.ModalToolBarContainer>
-          <ModalToolBar />
+          <ModalToolBar setModalsState={setModalsState} />
         </S.ModalToolBarContainer>
         <S.ContentContainer>
           <SessionModal />
+          <PertinentFactModal />
+          <AttachmentModal />
+          <PsychologicalAssessmentModal />
+          <GoalsRequirementsModal />
+          <PersonalNotesModal />
         </S.ContentContainer>
       </S.Grid>
-    </div>
+    </ModalContext.Provider>
   );
 };
 
