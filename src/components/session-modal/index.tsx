4f71f10a05e2604d4a99cd295fb09f23fb9bece0
cesarhenrq <cesaarhvb@gmail.com>
2023-04-04 04:21:56 -0300

    feat: implement session modal component

--- src/components/session-modal/index.tsx
+++ src/components/session-modal/index.tsx
@@ -1,42 +1,206 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
-import TitleModal from '../title-modal';
-import ModalFooter from '../modal-footer';
+import { useContext } from 'react';
+import { useForm, DefaultValues } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { schema } from './schema';
+import { ModalContext } from '../../contexts/ModalContext';
+import {
+  TextField,
+  InputLabel,
+  FormGroup,
+  Grid,
+  Box,
+  Select,
+  MenuItem,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  Typography,
+} from '@mui/material';
+import ModalBaseLayout from '../modal-base-layout';
+import Circle from '../circle';
+import GroupLabelContainer from '../form-label-group-container';
+import Divider from '@mui/material/Divider';
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 674,
-  bgcolor: 'background.paper',
-  border: '2px solid secondary.dark',
-  boxShadow: 24,
-  borderRadius: 2,
-  p: '24px 32px 0 32px',
+type SessionFormType = {
+  date: string;
+  initialHour: string;
+  finalHour: string;
+  title: string;
+  sessionResume: string;
+  price: string;
+  paymentMethod: string;
+  paymentStatus: string;
+};
+
+const defaultValues: DefaultValues<SessionFormType> = {
+  date: '',
+  initialHour: '',
+  finalHour: '',
+  title: '',
+  sessionResume: '',
+  price: '',
+  paymentMethod: '',
+  paymentStatus: '',
 };
 
 const SessionModal = () => {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const { modalsState } = useContext(ModalContext);
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<SessionFormType>({
+    defaultValues,
+    resolver: yupResolver(schema),
+    mode: 'onBlur',
+  });
+  const onSubmit = (data: SessionFormType) => {
+    return data;
+  };
 
   return (
-    <div>
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="session-modal-title"
-      >
-        <Box sx={style}>
-          <TitleModal title="Nova Sessão" />
-          <ModalFooter />
-        </Box>
-      </Modal>
-    </div>
+    <ModalBaseLayout
+      title="Nova Sessão"
+      modalState={modalsState.isSessionModalOpen}
+      modal="isSessionModalOpen"
+    >
+      <Box sx={{ maxHeight: 'calc(100vh - 250px)', overflowY: 'scroll' }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <GroupLabelContainer>
+            <Circle>
+              <Typography variant="h6">1</Typography>
+            </Circle>
+            <Typography variant="h6">Dados Gerais</Typography>
+          </GroupLabelContainer>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={4}>
+              <FormGroup>
+                <InputLabel htmlFor="date-input">Data*</InputLabel>
+                <TextField
+                  id="date-input"
+                  {...register('date')}
+                  error={Boolean(errors.date)}
+                />
+              </FormGroup>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormGroup>
+                <InputLabel htmlFor="initial-hour-input">
+                  Hora de início*
+                </InputLabel>
+                <TextField
+                  id="initial-hour-input"
+                  {...register('initialHour')}
+                  error={Boolean(errors.initialHour)}
+                />
+              </FormGroup>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormGroup>
+                <InputLabel htmlFor="final-hour-input">Hora fim*</InputLabel>
+                <TextField
+                  id="final-hour-input"
+                  {...register('finalHour')}
+                  error={Boolean(errors.finalHour)}
+                />
+              </FormGroup>
+            </Grid>
+          </Grid>
+          <Divider sx={{ mt: 3 }} />
+          <GroupLabelContainer>
+            <Circle>
+              <Typography variant="h6">2</Typography>
+            </Circle>
+            <Typography variant="h6">Sessão</Typography>
+          </GroupLabelContainer>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <FormGroup>
+                <InputLabel htmlFor="title-input">Título*</InputLabel>
+                <TextField
+                  id="title-input"
+                  {...register('title')}
+                  error={Boolean(errors.title)}
+                />
+              </FormGroup>
+            </Grid>
+            <Grid item xs={12}>
+              <Box sx={{ width: '100%' }}>
+                <FormGroup>
+                  <InputLabel htmlFor="session-resume-input">
+                    Resumo da sessão*
+                  </InputLabel>
+                  <TextField
+                    id="session-resume-input"
+                    multiline
+                    rows={5}
+                    placeholder="Text"
+                    {...register('sessionResume')}
+                    error={Boolean(errors.sessionResume)}
+                  />
+                </FormGroup>
+              </Box>
+            </Grid>
+          </Grid>
+          <Divider sx={{ mt: 3 }} />
+          <GroupLabelContainer>
+            <Circle>
+              <Typography variant="h6">3</Typography>
+            </Circle>
+            <Typography variant="h6">Pagamento</Typography>
+          </GroupLabelContainer>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={4}>
+              <FormGroup>
+                <InputLabel htmlFor="price-input">Valor</InputLabel>
+                <TextField
+                  id="price-input"
+                  {...register('price')}
+                  error={Boolean(errors.price)}
+                />
+              </FormGroup>
+            </Grid>
+
+            <Grid item xs={12} sm={4}>
+              <FormGroup>
+                <InputLabel htmlFor="payment-method-input">
+                  Forma de pagamento
+                </InputLabel>
+                <Select
+                  id="payment-method-input"
+                  {...register('paymentMethod')}
+                  error={Boolean(errors.paymentMethod)}
+                >
+                  <MenuItem value="pix">PIX</MenuItem>
+                </Select>
+              </FormGroup>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormGroup>
+                <InputLabel htmlFor="payment-status-input">Status</InputLabel>
+                <RadioGroup
+                  row
+                  id="payment-status-input"
+                  {...register('paymentStatus')}
+                >
+                  <FormControlLabel
+                    value="payed"
+                    control={<Radio />}
+                    label="Pago"
+                  />
+                  <FormControlLabel
+                    value="notPayed"
+                    control={<Radio />}
+                    label="Não pago"
+                  />
+                </RadioGroup>
+              </FormGroup>
+            </Grid>
+          </Grid>
+        </form>
+      </Box>
+    </ModalBaseLayout>
   );
 };
 
