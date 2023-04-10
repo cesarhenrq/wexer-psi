3e7b1a430decfac3cebcadea14689cfc7e59cfaf
cesarhenrq <cesaarhvb@gmail.com>
2023-04-10 20:06:32 -0300

    feat: implement attachment modal layout

--- src/components/attachment-modal/index.tsx
+++ src/components/attachment-modal/index.tsx
@@ -1,16 +1,126 @@
-import { useContext } from 'react';
+import { useContext, useState, ChangeEvent } from 'react';
+import { useForm, DefaultValues } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { schema } from './schema';
+import {
+  TextField,
+  InputLabel,
+  FormGroup,
+  Grid,
+  Box,
+  Button,
+  Typography,
+} from '@mui/material';
 import { ModalContext } from '../../contexts/ModalContext';
 import ModalBaseLayout from '../modal-base-layout';
 
+type AttachmentModalFormType = {
+  date: string;
+  title: string;
+  description: string;
+};
+
+const defaultValues: DefaultValues<AttachmentModalFormType> = {
+  date: '',
+  title: '',
+  description: '',
+};
+
 const AttachmentModal = () => {
   const { modalsState } = useContext(ModalContext);
+
+  const [fileName, setFileName] = useState('');
+
+  const handleFileUpload = (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    files ? setFileName(files[0].name) : setFileName('');
+  };
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<AttachmentModalFormType>({
+    defaultValues,
+    resolver: yupResolver(schema),
+    mode: 'onBlur',
+  });
+
+  const onSubmit = (data: AttachmentModalFormType) => {
+    return data;
+  };
+
   return (
     <ModalBaseLayout
       title="Novo Anexo"
       modalState={modalsState.isAttachmentModalOpen}
       modal="isAttachmentModalOpen"
     >
-      <p>Modal Sessão</p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={2} sx={{ mb: 3 }}>
+          <Grid item xs={12} sm={3}>
+            <FormGroup>
+              <InputLabel htmlFor="date-input">Data*</InputLabel>
+              <TextField
+                id="date-input"
+                {...register('date')}
+                error={Boolean(errors.date)}
+              />
+            </FormGroup>
+          </Grid>
+          <Grid item xs={12} sm={9}>
+            <FormGroup>
+              <InputLabel htmlFor="title-input">Título*</InputLabel>
+              <TextField
+                id="title-input"
+                placeholder="Digite"
+                {...register('title')}
+                error={Boolean(errors.title)}
+              />
+            </FormGroup>
+          </Grid>
+          <Grid item xs={12}>
+            <Box sx={{ width: '100%' }}>
+              <FormGroup>
+                <InputLabel htmlFor="description-input">Descrição*</InputLabel>
+                <TextField
+                  id="description-input"
+                  multiline
+                  rows={5}
+                  placeholder="Text"
+                  {...register('description')}
+                  error={Boolean(errors.description)}
+                />
+              </FormGroup>
+            </Box>
+          </Grid>
+          <Grid item xs={12}>
+            <FormGroup>
+              <InputLabel htmlFor="attachment-input">
+                Anexar arquivos*
+              </InputLabel>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Button
+                  variant="contained"
+                  component="label"
+                  sx={{ textTransform: 'none', width: 'fit-content', mr: 2 }}
+                >
+                  Escolher arquivos...
+                  <input
+                    id="attachment-input"
+                    hidden
+                    accept="image/*"
+                    multiple
+                    type="file"
+                    onChange={handleFileUpload}
+                  />
+                </Button>
+                <Typography color="secondary.dark">{fileName}</Typography>
+              </div>
+            </FormGroup>
+          </Grid>
+        </Grid>
+      </form>
     </ModalBaseLayout>
   );
 };
