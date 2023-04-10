aae4f9b515c0f9c8fc98b849e17d03a412a9903d
cesarhenrq <cesaarhvb@gmail.com>
2023-04-10 20:18:07 -0300

    refactor: cleaned the code

--- src/components/tab-nav/styles.ts
+++ src/components/tab-nav/styles.ts
@@ -3,14 +3,3 @@ import styled from 'styled-components';
 export const Container = styled.div`
   margin: 27px 84px 0px 58px;
 `;
-
-export const SearchInputContainer = styled.div`
-  width: 386px;
-  border: 1px solid #e0e0e0;
-  border-radius: 8px;
-  margin: 0 24px 0 0;
-
-  input::placeholder {
-    text-align: center;
-  }
-`;
