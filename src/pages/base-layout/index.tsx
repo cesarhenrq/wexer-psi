eac2361b8eb79e80baea11e5679e43423f4f21d4
cesarhenrq <cesaarhvb@gmail.com>
2023-04-10 20:12:14 -0300

    feat: add side bar context provider, sidebar drawer

--- src/pages/base-layout/index.tsx
+++ src/pages/base-layout/index.tsx
@@ -1,21 +1,29 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../../components/header';
 import SideBar from '../../components/side-bar';
+import SideBarDrawer from '../../components/side-bar-drawer';
+import { SideBarContext } from '../../contexts/SideBarContext';
 import * as S from './styles';
 
 const BaseLayout = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <S.BaseLayout>
-      <S.Header>
-        <Header />
-      </S.Header>
-      <S.Sidebar>
-        <SideBar />
-      </S.Sidebar>
-      <S.Content>
-        <Outlet />
-      </S.Content>
-    </S.BaseLayout>
+    <SideBarContext.Provider value={{ setIsOpen, isOpen }}>
+      <S.BaseLayout>
+        <S.Header>
+          <Header />
+        </S.Header>
+        <S.Sidebar>
+          <SideBarDrawer />
+          <SideBar />
+        </S.Sidebar>
+        <S.Content>
+          <Outlet />
+        </S.Content>
+      </S.BaseLayout>
+    </SideBarContext.Provider>
   );
 };
 
