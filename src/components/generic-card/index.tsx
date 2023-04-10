c43dc7ced3c5af7fc1c6b9a87c5fdf816a9cf1a7
cesarhenrq <cesaarhvb@gmail.com>
2023-04-10 20:14:08 -0300

    change: adapt component to new app logic

--- src/components/generic-card/index.tsx
+++ src/components/generic-card/index.tsx
@@ -1,39 +1,20 @@
-import { Dispatch, SetStateAction } from 'react';
-import { Card, CardContent, Typography, Button } from '@mui/material';
-import EditIcon from '../edit-icon';
+import { Card, CardContent, Typography } from '@mui/material';
+import EditButton from '../edit-button';
 import * as S from './styles';
 
 type GenericCardProps = {
   title: string;
   content: string;
   modal: ModalsStateKeys;
-  setModalsState: Dispatch<SetStateAction<ModalsStateT>>;
 };
 
-const GenericCard = ({
-  title,
-  content,
-  modal,
-  setModalsState,
-}: GenericCardProps) => {
-  const handleClick = <Keys extends ModalsStateKeys>(key: Keys) => {
-    setModalsState((prevValue) => ({
-      ...prevValue,
-      [key]: true,
-    }));
-  };
+const GenericCard = ({ title, content, modal }: GenericCardProps) => {
   return (
     <Card>
       <CardContent>
         <S.Title>
           <Typography variant="h6">{title}</Typography>
-          <Button
-            variant="outlined"
-            sx={{ width: 32, height: 24, padding: 0 }}
-            onClick={() => handleClick(modal)}
-          >
-            <EditIcon />
-          </Button>
+          <EditButton modal={modal} />
         </S.Title>
         <div>{content}</div>
       </CardContent>
