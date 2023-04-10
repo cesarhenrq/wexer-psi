0e448eff6f33fa567d296c152fb7db3d72f5eef9
cesarhenrq <cesaarhvb@gmail.com>
2023-04-10 20:16:22 -0300

    feat: add logic to component

--- src/components/side-bar/index.tsx
+++ src/components/side-bar/index.tsx
@@ -1,3 +1,6 @@
+import { useContext } from 'react';
+import { useTheme } from '@mui/material/styles';
+import { SideBarContext } from '../../contexts/SideBarContext';
 import PeopleIcon from '../people-icon';
 import ClipboardIcon from '../clipboard-icon';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
@@ -5,18 +8,21 @@ import ConfigureIcon from '../configure-icon';
 import * as S from './styles';
 
 const SideBar = () => {
+  const { setIsOpen } = useContext(SideBarContext);
+  const theme = useTheme();
+
   return (
-    <S.SideBar>
-      <S.Icon style={{ marginTop: 25 }}>
+    <S.SideBar theme={theme}>
+      <S.Icon style={{ marginTop: 25 }} onClick={() => setIsOpen(true)}>
         <PeopleIcon />
       </S.Icon>
-      <S.Icon>
+      <S.Icon onClick={() => setIsOpen(true)}>
         <ClipboardIcon color="white" width="24" height="24" />
       </S.Icon>
-      <S.Icon>
+      <S.Icon onClick={() => setIsOpen(true)}>
         <AddCircleIcon color="secondary" />
       </S.Icon>
-      <S.Icon>
+      <S.Icon onClick={() => setIsOpen(true)}>
         <ConfigureIcon />
       </S.Icon>
     </S.SideBar>
