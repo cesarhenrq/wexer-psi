0a987de8b56222e64d819ec86f70baf6d3404fea
cesarhenrq <cesaarhvb@gmail.com>
2023-04-10 20:13:24 -0300

    feat: add edit button logic

--- src/components/edit-button/index.tsx
+++ src/components/edit-button/index.tsx
@@ -1,10 +1,31 @@
+import { useContext } from 'react';
+import { ModalContext } from '../../contexts/ModalContext';
 import Button from '@mui/material/Button';
 import EditIcon from '../edit-icon';
 
-const EditButton = () => (
-  <Button variant="outlined" sx={{ width: 32, height: 24, padding: 0 }}>
-    <EditIcon />
-  </Button>
-);
+type EditButtonProps = {
+  modal: ModalsStateKeys;
+};
+
+const EditButton = ({ modal }: EditButtonProps) => {
+  const { setModalsState } = useContext(ModalContext);
+
+  const handleClick = <Keys extends ModalsStateKeys>(key: Keys) => {
+    setModalsState((prevValue) => ({
+      ...prevValue,
+      [key]: true,
+    }));
+  };
+
+  return (
+    <Button
+      variant="outlined"
+      sx={{ width: 32, height: 24, padding: 0 }}
+      onClick={() => handleClick(modal)}
+    >
+      <EditIcon />
+    </Button>
+  );
+};
 
 export default EditButton;
