e4d55f2b2d465f74437419631de4a9b14f09b362
cesarhenrq <cesaarhvb@gmail.com>
2023-04-10 20:17:00 -0300

    feat: add theme logic to sidebar style

--- src/components/side-bar/styles.ts
+++ src/components/side-bar/styles.ts
@@ -6,35 +6,9 @@ export const SideBar = styled.div`
   align-items: center;
   width: 80px;
   height: 100vh;
-  background-color: #017849;
+  background-color: ${({ theme }) => theme.palette.primary.main};
 `;
 
 export const Icon = styled.div`
   margin: 0 0 33.17px 0;
 `;
-
-/*<List>
-        <ListItem alignItems="center">
-          <ListItemIcon>
-            <PeopleIcon />
-          </ListItemIcon>
-        </ListItem>
-
-        <ListItem alignItems="center">
-          <ListItemIcon>
-            <ClipboardIcon />
-          </ListItemIcon>
-        </ListItem>
-
-        <ListItem alignItems="center">
-          <ListItemIcon>
-            <AddCircleIcon color="secondary" />
-          </ListItemIcon>
-        </ListItem>
-
-        <ListItem alignItems="center">
-          <ListItemIcon>
-            <ConfigureIcon />
-          </ListItemIcon>
-        </ListItem>
-      </List>*/
