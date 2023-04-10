fdd39ad7fa9acf73b612855623e3fde14a8c400c
cesarhenrq <cesaarhvb@gmail.com>
2023-04-10 20:12:38 -0300

    feat: add modal context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
+import { ModalContext } from './contexts/ModalContext';
 import { Normalize } from 'styled-normalize';
 import Router from './routes';
 import theme from './theme';
 
 function App() {
+  const [modalsState, setModalsState] = useState<ModalsStateT>({
+    isSessionModalOpen: false,
+    isPertinentFactModalOpen: false,
+    isAttachmentModalOpen: false,
+    isPsychologicalAssessmentModalOpen: false,
+    isGoalsRequirementsModalOpen: false,
+    isPersonalNotesModalOpen: false,
+    isDeleteModalOpen: false,
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <Normalize />
-        <Router />
-      </div>
+      <ModalContext.Provider value={{ setModalsState, modalsState }}>
+        <div className="App">
+          <Normalize />
+          <Router />
+        </div>
+      </ModalContext.Provider>
     </ThemeProvider>
   );
 }
