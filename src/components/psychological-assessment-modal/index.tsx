c8262b7c0f868fd9aa848af3e09055e78835626a
cesarhenrq <cesaarhvb@gmail.com>
2023-04-14 04:40:55 -0300

    feat: add schema to psychological assessment modal

--- src/components/psychological-assessment-modal/index.tsx
+++ src/components/psychological-assessment-modal/index.tsx
@@ -1,16 +1,86 @@
 import { useContext } from 'react';
 import { ModalContext } from '../../contexts/ModalContext';
+import { useForm, DefaultValues } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { schema } from './schema';
+import {
+  TextField,
+  InputLabel,
+  FormGroup,
+  Grid,
+  Box,
+  Typography,
+} from '@mui/material';
 import ModalBaseLayout from '../modal-base-layout';
+
+type PsychologicalAssessmentFormType = {
+  date: string;
+  title: string;
+  description: string;
+};
+
+const defaultValues: DefaultValues<PsychologicalAssessmentFormType> = {
+  date: '',
+};
 
 const PsychologicalAssessmentModal = () => {
   const { modalsState } = useContext(ModalContext);
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<PsychologicalAssessmentFormType>({
+    defaultValues,
+    resolver: yupResolver(schema),
+    mode: 'onBlur',
+  });
+
+  const onSubmit = (data: PsychologicalAssessmentFormType) => {
+    return data;
+  };
+
   return (
     <ModalBaseLayout
       title="Nova Avaliação Psicológica"
       modalState={modalsState.isPsychologicalAssessmentModalOpen}
       modal="isPsychologicalAssessmentModalOpen"
+      buttonTitle="Prosseguir"
     >
-      <p>Modal Sessão</p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={2} sx={{ mb: 3 }}>
+          <Grid item xs={12} sm={3}>
+            <FormGroup>
+              <InputLabel htmlFor="date-input">Data*</InputLabel>
+              <TextField
+                id="date-input"
+                {...register('date')}
+                error={Boolean(errors.date)}
+              />
+            </FormGroup>
+          </Grid>
+          <Grid item xs={12}>
+            <Grid
+              item
+              xs={12}
+              sx={{ bgcolor: 'primary.light', borderRadius: 2, p: 2 }}
+            >
+              <Box sx={{ width: '100%' }}>
+                <Typography color="primary" sx={{ fontWeight: 'bold' }}>
+                  Atenção!
+                </Typography>
+                <Typography>
+                  Você será{' '}
+                  <b>
+                    <i>redirecionado para uma nova página</i>
+                  </b>{' '}
+                  onde irá preencher os dados da avaliação psicológica.
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+        </Grid>
+      </form>
     </ModalBaseLayout>
   );
 };
