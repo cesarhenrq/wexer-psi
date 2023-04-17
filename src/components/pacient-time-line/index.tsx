489ac1bf1aac4d18fa7514012862886ebf8c4380
César Henrique <cesaarhvb@gmail.com>
2023-04-17 17:13:41 -0300

    feat: add paciente time line

---
+++ src/components/pacient-time-line/index.tsx
@@ -0,0 +1,13 @@
+import TimeLineItem from '../time-line-item';
+import VerticalDivider from '../vertical-divider';
+
+const PacientTimeLine = () => {
+  return (
+    <>
+      <VerticalDivider />
+      <TimeLineItem type="session" />
+    </>
+  );
+};
+
+export default PacientTimeLine;
