80c2a82807c02184f3e6f3f117c771234ba03efc
cesarhenrq <cesaarhvb@gmail.com>
2023-04-18 23:01:42 -0300

    feat: add filling statement

--- src/components/pin-icon/index.tsx
+++ src/components/pin-icon/index.tsx
@@ -1,4 +1,8 @@
-const PinIcon = () => {
+type PinIconProps = {
+  filled: boolean;
+};
+
+const PinIcon = ({ filled }: PinIconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -9,7 +13,9 @@ const PinIcon = () => {
     >
       <g clipPath="url(#clip0_4022_24909)">
         <path
-          fill="#2F80ED"
+          fill={filled ? '#2F80ED' : 'none'}
+          stroke={filled ? 'none' : '#fff'}
+          strokeWidth="2"
           d="M29.752 13.563l-1.887 1.885-.942-.943-5.656 5.656-.943 4.715-1.887 1.885-5.656-5.657-6.6 6.6-1.885-1.885 6.6-6.6-5.657-5.656 1.885-1.887 4.716-.943 5.656-5.656-.943-.942 1.886-1.887 11.313 11.315z"
         ></path>
       </g>
