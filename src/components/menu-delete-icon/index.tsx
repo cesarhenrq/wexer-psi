f4a6710802afcbdc458414bc044bc1d155b48979
cesarhenrq <cesaarhvb@gmail.com>
2023-04-20 18:11:24 -0300

    feat: implement menu delete icon

---
+++ src/components/menu-delete-icon/index.tsx
@@ -0,0 +1,16 @@
+const MenuDeleteIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    fill="none"
+    viewBox="0 0 20 20"
+  >
+    <path
+      fill="#EB5757"
+      d="M2 6h16v13a1 1 0 01-1 1H3a1 1 0 01-1-1V6zm2 2v10h12V8H4zm3 2h2v6H7v-6zm4 0h2v6h-2v-6zM5 3V1a1 1 0 011-1h8a1 1 0 011 1v2h5v2H0V3h5zm2-1v1h6V2H7z"
+    ></path>
+  </svg>
+);
+
+export default MenuDeleteIcon;
