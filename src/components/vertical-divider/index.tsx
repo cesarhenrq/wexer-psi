ea9a8d643a0436ebff9caf5a0f951aac14d62cd9
cesarhenrq <cesaarhvb@gmail.com>
2023-04-20 19:31:56 -0300

    refactor: cleaned the code

--- src/components/vertical-divider/index.tsx
+++ src/components/vertical-divider/index.tsx
@@ -15,38 +15,38 @@ type VerticalDividerProps = {
 };
 
 const VerticalDivider = ({ color, type }: VerticalDividerProps) => {
+  const icon =
+    type === 'session' ? (
+      <SessionIcon filled={false} />
+    ) : type === 'attachment' ? (
+      <ClipIcon filled={false} />
+    ) : type === 'pertinent-fact' ? (
+      <PinIcon filled={false} />
+    ) : (
+      <ClipboardLinedIcon />
+    );
   return (
-    <>
-      <Box sx={{ position: 'relative' }}>
-        <VerticalLine color={color} />
-        <Box
-          sx={{
-            position: 'absolute',
-            top: '100%',
-            left: '3.5%',
-            transform: 'translate(-50%, -50%)',
-            width: '48px',
-            height: '48px',
-            borderRadius: '50%',
-            backgroundColor: `${color}`,
-            zIndex: 1,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          {type === 'session' ? (
-            <SessionIcon filled={false} />
-          ) : type === 'attachment' ? (
-            <ClipIcon filled={false} />
-          ) : type === 'pertinent-fact' ? (
-            <PinIcon filled={false} />
-          ) : (
-            <ClipboardLinedIcon />
-          )}
-        </Box>
+    <Box sx={{ position: 'relative' }}>
+      <VerticalLine color={color} />
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '100%',
+          left: '3.5%',
+          transform: 'translate(-50%, -50%)',
+          width: '48px',
+          height: '48px',
+          borderRadius: '50%',
+          backgroundColor: `${color}`,
+          zIndex: 1,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {icon}
       </Box>
-    </>
+    </Box>
   );
 };
 
