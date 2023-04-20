c7f2808b5b1a75d683871fb4251aeb36d0a55c91
cesarhenrq <cesaarhvb@gmail.com>
2023-04-20 19:28:10 -0300

    refactor: cleaned the code

--- src/components/clipboard-lined-icon/index.tsx
+++ src/components/clipboard-lined-icon/index.tsx
@@ -1,18 +1,16 @@
-const ClipboardLinedIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="30"
-      fill="none"
-      viewBox="0 0 24 30"
-    >
-      <path
-        fill="#fff"
-        d="M18.667 2.667h4A1.333 1.333 0 0124 4v24a1.333 1.333 0 01-1.333 1.333H1.333A1.333 1.333 0 010 28V4a1.333 1.333 0 011.333-1.333h4V0H8v2.667h8V0h2.667v2.667zm0 2.666V8H16V5.333H8V8H5.333V5.333H2.667v21.334h18.666V5.333h-2.666zM5.333 10.667h13.334v2.666H5.333v-2.666zm0 5.333h13.334v2.667H5.333V16z"
-      ></path>
-    </svg>
-  );
-};
+const ClipboardLinedIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="30"
+    fill="none"
+    viewBox="0 0 24 30"
+  >
+    <path
+      fill="#fff"
+      d="M18.667 2.667h4A1.333 1.333 0 0124 4v24a1.333 1.333 0 01-1.333 1.333H1.333A1.333 1.333 0 010 28V4a1.333 1.333 0 011.333-1.333h4V0H8v2.667h8V0h2.667v2.667zm0 2.666V8H16V5.333H8V8H5.333V5.333H2.667v21.334h18.666V5.333h-2.666zM5.333 10.667h13.334v2.666H5.333v-2.666zm0 5.333h13.334v2.667H5.333V16z"
+    ></path>
+  </svg>
+);
 
 export default ClipboardLinedIcon;
