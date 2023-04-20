92aff6584b09bf50614b7af3239e66999164d2c1
cesarhenrq <cesaarhvb@gmail.com>
2023-04-20 18:11:42 -0300

    feat: implement menu edit icon

---
+++ src/components/menu-edit-icon/index.tsx
@@ -0,0 +1,16 @@
+const MenuEditIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="18"
+    height="18"
+    fill="none"
+    viewBox="0 0 18 18"
+  >
+    <path
+      fill="#2F80ED"
+      d="M12.728 6.686l-1.414-1.414L2 14.586V16h1.414l9.314-9.314zm1.414-1.414l1.414-1.414-1.414-1.414-1.414 1.414 1.414 1.414zM4.242 18H0v-4.243L13.435.322a1 1 0 011.414 0l2.829 2.83a1 1 0 010 1.413L4.243 18h-.001z"
+    ></path>
+  </svg>
+);
+
+export default MenuEditIcon;
