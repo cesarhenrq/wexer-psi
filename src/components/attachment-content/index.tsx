fcc4562ece66da102d77ff203fa5b28d33b994a8
cesarhenrq <cesaarhvb@gmail.com>
2023-04-20 19:26:26 -0300

    refactor: cleaned the code

--- src/components/attachment-content/index.tsx
+++ src/components/attachment-content/index.tsx
@@ -1,5 +1,4 @@
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import { Typography, Box } from '@mui/material';
 import PdfIcon from '../pdf-icon';
 
 type AttachmentContentProps = {
@@ -10,25 +9,30 @@ type AttachmentContentProps = {
 const AttachmentContent = ({
   text,
   numberOfAttachments,
-}: AttachmentContentProps) => (
-  <Box>
-    <Typography variant="body1" mt={1} mb={1}>
-      {text}
-    </Typography>
-    <Typography variant="caption" mb={1}>
-      <b>{`${numberOfAttachments} Anexos`}</b>
-    </Typography>
-    <a
-      href="caminho_do_arquivo.pdf"
-      download
-      style={{ display: 'flex', alignItems: 'center', color: '#2F80ED' }}
-    >
-      <PdfIcon />
-      <Typography ml={0.5} variant="subtitle2">
-        <b>caminho_do_arquivo.pdf(233 KB)</b>
+}: AttachmentContentProps) => {
+  const filePath = 'caminho_do_arquivo.pdf';
+  const fileSize = '233 KB';
+
+  return (
+    <Box>
+      <Typography variant="body1" mt={1} mb={1}>
+        {text}
       </Typography>
-    </a>
-  </Box>
-);
+      <Typography variant="caption" mb={1}>
+        <b>{`${numberOfAttachments} Anexos`}</b>
+      </Typography>
+      <a
+        href={filePath}
+        download
+        style={{ display: 'flex', alignItems: 'center', color: '#2F80ED' }}
+      >
+        <PdfIcon />
+        <Typography ml={0.5} variant="subtitle2">
+          <b>{`${filePath} (${fileSize})`}</b>
+        </Typography>
+      </a>
+    </Box>
+  );
+};
 
 export default AttachmentContent;
