c925d209729b4efb94d50167596e97d515e143a4
cesarhenrq <cesaarhvb@gmail.com>
2023-04-20 18:11:52 -0300

    feat: implement menu export icon

---
+++ src/components/menu-export-icon/index.tsx
@@ -0,0 +1,16 @@
+const MenuExportIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="18"
+    height="18"
+    fill="none"
+    viewBox="0 0 18 18"
+  >
+    <path
+      fill="#00995D"
+      d="M7 3v2H2v11h11v-5h2v6a1 1 0 01-1 1H1a1 1 0 01-1-1V4a1 1 0 011-1h6zm11-3v8h-2V3.413l-7.793 7.794-1.414-1.414L14.585 2H10V0h8z"
+    ></path>
+  </svg>
+);
+
+export default MenuExportIcon;
