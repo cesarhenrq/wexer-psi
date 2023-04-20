d0f79f0f61f2b6e887b760bc65465e68a7490d21
cesarhenrq <cesaarhvb@gmail.com>
2023-04-19 21:06:39 -0300

    feat: add time line item container

--- src/components/time-line-item/index.tsx
+++ src/components/time-line-item/index.tsx
@@ -4,6 +4,9 @@ import Typography from '@mui/material/Typography';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import SessionContent from '../session-content';
 import VerticalDivider from '../vertical-divider';
+import * as S from './styles';
+import PertinentFactContent from '../pertinent-fact-content';
+import AttachmentContent from '../attachment-content';
 
 type TimeLineItemProps = {
   type:
@@ -19,14 +22,14 @@ const TimeLineItem = ({ type }: TimeLineItemProps) => {
     type === 'session'
       ? (color = '#00995D')
       : type === 'attachment'
-      ? (color = '#2F80ED')
-      : type === 'pertinent-fact'
       ? (color = '#9D28AC')
+      : type === 'pertinent-fact'
+      ? (color = '#2F80ED')
       : (color = '#EA1E61');
     return color;
   };
   return (
-    <>
+    <S.TimeLineItemContainer>
       <VerticalDivider color={handleColor()} type={type} />
       <Card sx={{ borderLeft: `4px solid ${handleColor()}`, pt: 2 }}>
         <CardContent>
@@ -41,13 +44,21 @@ const TimeLineItem = ({ type }: TimeLineItemProps) => {
             Sessão 02
             <MoreHorizIcon />
           </Typography>
-          <Typography sx={{ mb: 1.5 }} color="secondary.dark">
+          <Typography variant="caption" color="secondary.dark">
             22 de setembro de 2022
           </Typography>
-          {type === 'session' ? <SessionContent content="it works" /> : null}
+          {type === 'session' ? (
+            <SessionContent content="it works" />
+          ) : type === 'attachment' ? (
+            <AttachmentContent text="text" numberOfAttachments={2} />
+          ) : type === 'pertinent-fact' ? (
+            <PertinentFactContent content="it works 3!" />
+          ) : (
+            <div> it works 4!</div>
+          )}
         </CardContent>
       </Card>
-    </>
+    </S.TimeLineItemContainer>
   );
 };
 
---
+++ src/components/time-line-item/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const TimeLineItemContainer = styled.div`
+  margin-top: 50px;
+`;
