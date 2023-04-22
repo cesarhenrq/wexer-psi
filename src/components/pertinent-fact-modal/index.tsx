3159bcfc2c1fe9b3724d15e9b06d5a7ab53c7b4a
cesarhenrq <cesaarhvb@gmail.com>
2023-04-21 22:34:38 -0300

    feat: add styled component

--- src/components/pertinent-fact-modal/index.tsx
+++ src/components/pertinent-fact-modal/index.tsx
@@ -2,9 +2,10 @@ import { useContext } from 'react';
 import { useForm, DefaultValues } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { schema } from './schema';
-import { TextField, InputLabel, FormGroup, Grid, Box } from '@mui/material';
+import { InputLabel, FormGroup, Grid, Box, Typography } from '@mui/material';
 import { ModalContext } from '../../contexts/ModalContext';
 import ModalBaseLayout from '../modal-base-layout';
+import * as S from './styles';
 
 type PertinentFactFormType = {
   date: string;
@@ -45,8 +46,10 @@ const PertinentFactModal = () => {
         <Grid container spacing={2} sx={{ mb: 3 }}>
           <Grid item xs={12} sm={3}>
             <FormGroup>
-              <InputLabel htmlFor="date-input">Data*</InputLabel>
-              <TextField
+              <InputLabel htmlFor="date-input">
+                <Typography variant="caption">Data*</Typography>
+              </InputLabel>
+              <S.PertinentFactTextField
                 id="date-input"
                 {...register('date')}
                 error={Boolean(errors.date)}
@@ -55,8 +58,10 @@ const PertinentFactModal = () => {
           </Grid>
           <Grid item xs={12} sm={9}>
             <FormGroup>
-              <InputLabel htmlFor="title-input">Título*</InputLabel>
-              <TextField
+              <InputLabel htmlFor="title-input">
+                <Typography variant="caption">Título*</Typography>
+              </InputLabel>
+              <S.PertinentFactTextField
                 id="title-input"
                 placeholder="Digite"
                 {...register('title')}
@@ -67,8 +72,10 @@ const PertinentFactModal = () => {
           <Grid item xs={12}>
             <Box sx={{ width: '100%' }}>
               <FormGroup>
-                <InputLabel htmlFor="description-input">Descrição*</InputLabel>
-                <TextField
+                <InputLabel htmlFor="description-input">
+                  <Typography variant="caption">Descrição*</Typography>
+                </InputLabel>
+                <S.PertinentFactTextField
                   id="description-input"
                   multiline
                   rows={5}
---
+++ src/components/pertinent-fact-modal/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { TextField } from '@mui/material';
+
+export const PertinentFactTextField = styled(TextField)`
+  & ::placeholder {
+    font-size: 14px;
+  }
+`;
