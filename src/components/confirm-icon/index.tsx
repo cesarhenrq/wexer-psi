d66345c595bfe1417065eccbf0aa734b3380d4f7
cesarhenrq <cesaarhvb@gmail.com>
2023-04-22 19:07:52 -0300

    feat: chore implement confirm icon component

---
+++ src/components/confirm-icon/index.tsx
@@ -0,0 +1,18 @@
+const ConfirmIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="14"
+    height="14"
+    fill="none"
+    viewBox="0 0 14 14"
+  >
+    <path
+      fill="#fff"
+      fillRule="evenodd"
+      d="M9.864 5.404l-3.045 4a.666.666 0 01-.526.263h-.005a.668.668 0 01-.525-.256L4.142 7.34a.666.666 0 111.05-.822l1.089 1.39 2.522-3.312a.667.667 0 011.061.808zM7.001.333a6.667 6.667 0 100 13.333A6.667 6.667 0 007 .333z"
+      clipRule="evenodd"
+    ></path>
+  </svg>
+);
+
+export default ConfirmIcon;
