336dd63c2122cea7e6d54d7b1de1e75843ff051a
cesarhenrq <cesaarhvb@gmail.com>
2023-04-23 00:11:40 -0300

    feat: implement top icon component

---
+++ src/components/top-icon/index.tsx
@@ -0,0 +1,18 @@
+const TopIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="14"
+    height="14"
+    fill="none"
+    viewBox="0 0 14 14"
+  >
+    <path
+      fill="#212121"
+      fillRule="evenodd"
+      d="M6.99 2.292L1.626 6.607A1 1 0 01.373 5.048l6-4.828A.998.998 0 017.64.232l6 5A.998.998 0 0113 7c-.226 0-.453-.077-.64-.232L6.99 2.292zm-.617 4.929a.998.998 0 011.267.01l6 5a1 1 0 01-1.28 1.537L6.99 9.293l-5.363 4.314a.999.999 0 11-1.254-1.558l6-4.828z"
+      clipRule="evenodd"
+    ></path>
+  </svg>
+);
+
+export default TopIcon;
