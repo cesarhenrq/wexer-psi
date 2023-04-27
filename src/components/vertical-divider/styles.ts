033c0d996fd807ef54c42629ea6cd4920a7cf64e
cesarhenrq <cesaarhvb@gmail.com>
2023-04-27 01:31:53 -0300

    feat: add some styles

--- src/components/vertical-divider/styles.ts
+++ src/components/vertical-divider/styles.ts
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 export const VerticalLine = styled.div`
   position: absolute;
   top: -50px;
-  left: 3.5%;
+  left: ${({ theme }) => theme.spacing(5)};
   z-index: 1;
   width: 2px;
   height: 50px;
-  background-color: ${(props) => props.color};
+  background-color: ${({ color }) => color};
   transform: rotate(180deg);
 `;
