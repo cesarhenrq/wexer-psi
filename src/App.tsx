383150f93ef837921dff97e1a9cee996379db671
cesarhenrq <cesaarhvb@gmail.com>
2023-04-27 01:29:51 -0300

    feat: add contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,48 @@
 import { ThemeProvider } from '@mui/material/styles';
-import { ModalContext } from './contexts/ModalContext';
 import { Normalize } from 'styled-normalize';
+import { login } from './services/user';
+import { PatientDataContext } from './contexts/PatientDataContext';
+import { ModalContext } from './contexts/ModalContext';
+import { ServicesContext } from './contexts/ServicesContext';
+import { ServiceContext } from './contexts/ServiceContext';
+import { OccurrencesContext } from './contexts/OccurrencesContext';
+import usePatientData from './hooks/use-patient-data';
+import useServices from './hooks/use-services';
+import useModal from './hooks/use-modal';
+import { useState } from 'react';
 import Router from './routes';
 import theme from './theme';
-import useModal from './hooks/use-modal';
-import { login } from './services/user';
 
 function App() {
   login();
 
+  const { patientData, setPatientData } = usePatientData();
+
   const { modalsState, setModalsState } = useModal();
+
+  const { services, setServices } = useServices();
+
+  const [service, setService] = useState({ serviceName: '', _id: '' });
+
+  const [occurrences, setOccurrences] = useState<OccurrenceType[]>([]);
 
   return (
     <ThemeProvider theme={theme}>
       <ModalContext.Provider value={{ setModalsState, modalsState }}>
-        <div className="App">
-          <Normalize />
-          <Router />
-        </div>
+        <PatientDataContext.Provider value={{ patientData, setPatientData }}>
+          <ServicesContext.Provider value={{ services, setServices }}>
+            <ServiceContext.Provider value={{ service, setService }}>
+              <OccurrencesContext.Provider
+                value={{ occurrences, setOccurrences }}
+              >
+                <div className="App">
+                  <Normalize />
+                  <Router />
+                </div>
+              </OccurrencesContext.Provider>
+            </ServiceContext.Provider>
+          </ServicesContext.Provider>
+        </PatientDataContext.Provider>
       </ModalContext.Provider>
     </ThemeProvider>
   );
