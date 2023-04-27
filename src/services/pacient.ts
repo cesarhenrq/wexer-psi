008789c684464463126e0bbb194b89e6387da325
cesarhenrq <cesaarhvb@gmail.com>
2023-04-27 01:23:33 -0300

    feat: add api integration

--- src/services/pacient.ts
+++ src/services/pacient.ts
@@ -1,10 +1,12 @@
 import { api } from './api';
-import { TOKEN_KEY, PACIENT_ID } from '../utils/constants';
+import { PACIENT_ID } from '../utils/constants';
+import getToken from '../utils/functions/get-token';
+
+type FieldType = 'demands' | 'personalAnnotations' | 'observation';
 
 export const getPacientData = async (): Promise<PatientType> => {
   try {
-    const token = localStorage.getItem(TOKEN_KEY);
-    if (!token) throw new Error('No token found');
+    const token = getToken();
     const result = await api.get(`/patient/${PACIENT_ID}`, {
       headers: { Authorization: token },
     });
@@ -17,6 +19,7 @@ export const getPacientData = async (): Promise<PatientType> => {
       demands,
       personalAnnotations,
     } = result.data;
+
     return {
       userId,
       name,
@@ -28,5 +31,23 @@ export const getPacientData = async (): Promise<PatientType> => {
     };
   } catch (error) {
     throw new Error('Failed to get user data');
+  }
+};
+
+export const updatePacientData = async (
+  field: FieldType,
+  updatedField: string
+): Promise<void> => {
+  try {
+    const token = getToken();
+    await api.put(
+      `/patient/${PACIENT_ID}`,
+      { [field]: updatedField },
+      {
+        headers: { Authorization: token },
+      }
+    );
+  } catch (error) {
+    throw new Error('Failed to update patient data');
   }
 };
