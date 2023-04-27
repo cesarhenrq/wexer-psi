7f0410278df272c18277dd6b74af4c055b37d248
cesarhenrq <cesaarhvb@gmail.com>
2023-04-27 01:18:45 -0300

    feat: add session type

--- src/types/Session.d.ts
+++ src/types/Session.d.ts
@@ -1,16 +1,4 @@
-type SessionType = {
-  occurrenceId: string;
-  timelineId: string;
-  createdOn: string;
-  title: string;
-  content: string;
-  date: string;
-  time: string;
-  timeline: Partial<TimeLineType>;
-  setTimeline: Dispatch<SetStateAction<Partial<TimeLineType>>>;
-  payment?: {
-    value: number;
-    method: string;
-    status: string;
-  };
-};
+type SessionType = Pick<
+  OccurrenceType,
+  '_id' | 'type' | 'title' | 'content' | 'payment' | 'createdOn' | 'timelineId'
+>;
