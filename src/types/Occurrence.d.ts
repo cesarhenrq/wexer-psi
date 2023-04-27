36e6678cb6cc6fa159c84cd862da0728325642c4
cesarhenrq <cesaarhvb@gmail.com>
2023-04-27 01:20:14 -0300

    feat: add occurrence type

--- src/types/Occurrence.d.ts
+++ src/types/Occurrence.d.ts
@@ -1,9 +1,20 @@
 type OccurrenceType = {
+  timelineId?: string;
   _id: string;
   title: string;
-  content: string;
-  files: [];
-  type: string;
+  content?: string;
+  files?: { filename: string; filesize: number }[];
+  type: TimeLineItemType;
+  payment?: {
+    value?: number;
+    method?: string;
+    status?: string;
+  };
+  assessments?: {
+    interview?: string;
+    tests?: string;
+    observation?: strin;
+  };
   createdOn: string;
-  modifiedOn: string;
+  modifiedOn?: string;
 };
