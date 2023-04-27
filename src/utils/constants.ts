8e09545bf9a2ee287085cb87649ef618bab0f929
cesarhenrq <cesaarhvb@gmail.com>
2023-04-27 01:17:51 -0300

    feat: add some constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,18 @@
 export const TOKEN_KEY = 'jwt';
 
 export const PACIENT_ID = '644863b80fafd9fcbd3a746b';
+
+const FORMATS = [
+  ['bold', 'italic', 'underline', 'strike'],
+  [{ script: 'sub' }, { script: 'super' }],
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  [{ indent: '-1' }, { indent: '+1' }],
+  ['align', { align: 'center' }, { align: 'right' }, { align: 'justify' }],
+  ['link'],
+];
+
+export const MODULES = {
+  toolbar: {
+    container: FORMATS,
+  },
+};
