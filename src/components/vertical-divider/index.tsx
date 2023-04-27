e129faf9d9f6e57f1a72d1e8b5f3bf68142ecf52
cesarhenrq <cesaarhvb@gmail.com>
2023-04-27 01:24:26 -0300

    feat: add stylization

--- src/components/vertical-divider/index.tsx
+++ src/components/vertical-divider/index.tsx
@@ -4,35 +4,34 @@ import SessionIcon from '../session-icon';
 import ClipIcon from '../clip-icon';
 import ClipboardLinedIcon from '../clipboard-lined-icon';
 import { VerticalLine } from './styles';
+import { useTheme } from '@mui/material';
 
 type VerticalDividerProps = {
   color: '#00995D' | '#2F80ED' | '#9D28AC' | '#EA1E61';
-  type:
-    | 'session'
-    | 'attachment'
-    | 'pertinent-fact'
-    | 'psychological-assessment';
+  type: TimeLineItemType;
+  index: number;
 };
 
-const VerticalDivider = ({ color, type }: VerticalDividerProps) => {
+const VerticalDivider = ({ color, type, index }: VerticalDividerProps) => {
+  const theme = useTheme();
   const icon =
     type === 'session' ? (
       <SessionIcon filled={false} />
     ) : type === 'attachment' ? (
       <ClipIcon filled={false} />
-    ) : type === 'pertinent-fact' ? (
+    ) : type === 'relevant_fact' ? (
       <PinIcon filled={false} />
     ) : (
       <ClipboardLinedIcon />
     );
   return (
     <Box sx={{ position: 'relative' }}>
-      <VerticalLine color={color} />
+      {index === 0 ? null : <VerticalLine color={color} theme={theme} />}
       <Box
         sx={{
           position: 'absolute',
           top: '100%',
-          left: '3.5%',
+          left: `${theme.spacing(5)}`,
           transform: 'translate(-50%, -50%)',
           width: '48px',
           height: '48px',
