8817ef5e82ea20827c697dc813bfd8820b4d99d8
cesarhenrq <cesaarhvb@gmail.com>
2023-04-29 19:27:35 -0300

    feat: add api integration

--- src/components/delete-modal/index.tsx
+++ src/components/delete-modal/index.tsx
@@ -2,13 +2,33 @@ import { useContext } from 'react';
 import { ModalContext } from '../../contexts/ModalContext';
 import { Modal, useTheme } from '@mui/material';
 import * as S from './styles';
+import { ServicesContext } from '../../contexts/ServicesContext';
+import { ServiceContext } from '../../contexts/ServiceContext';
+import { deleteService, getServices } from '../../services/service';
+import { SubmitingContext } from '../../contexts/SubmitingContext';
 
 const DeleteModal = () => {
   const { modalsState, setModalsState } = useContext(ModalContext);
+
+  const { setServices } = useContext(ServicesContext);
+
+  const { service, setService } = useContext(ServiceContext);
+
+  const { isSubmiting, setIsSubmiting } = useContext(SubmitingContext);
+
   const theme = useTheme();
 
   const handleClose = () =>
     setModalsState((prevValue) => ({ ...prevValue, isDeleteModalOpen: false }));
+
+  const handleDelete = async () => {
+    setIsSubmiting(true);
+    await deleteService(service._id);
+    const updatedServices = await getServices();
+    setServices(updatedServices);
+    setService({ serviceName: '', _id: '' });
+    setIsSubmiting(false);
+  };
 
   return (
     <Modal open={modalsState.isDeleteModalOpen} onClose={handleClose}>
@@ -25,7 +45,13 @@ const DeleteModal = () => {
           >
             Voltar
           </S.Buttons>
-          <S.Buttons theme={theme} variant="contained" color="error">
+          <S.Buttons
+            theme={theme}
+            variant="contained"
+            color="error"
+            onClick={handleDelete}
+            disabled={isSubmiting}
+          >
             Apagar
           </S.Buttons>
         </S.InnerBox>
