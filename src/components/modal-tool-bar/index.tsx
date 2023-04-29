5de9d1c96dfc195fc3845d32e1eb23b7cf1e3c93
cesarhenrq <cesaarhvb@gmail.com>
2023-04-29 19:17:39 -0300

    feat: add api integration

--- src/components/modal-tool-bar/index.tsx
+++ src/components/modal-tool-bar/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect } from 'react';
 import { ModalContext } from '../../contexts/ModalContext';
 import { Box, Typography, MenuItem, useTheme, Button } from '@mui/material';
 import * as S from './styles';
@@ -9,29 +9,56 @@ import ClipIcon from '../clip-icon';
 import ClipboardIcon from '../clipboard-icon';
 import EditButton from '../edit-button';
 import { SelectChangeEvent } from '@mui/material/Select';
+import { ServicesContext } from '../../contexts/ServicesContext';
+import { ServiceContext } from '../../contexts/ServiceContext';
+import { OccurrencesContext } from '../../contexts/OccurrencesContext';
+import { getOccurrences } from '../../services/occurrence';
+import { EditingContext } from '../../contexts/EditingContext';
 
 const ModalToolBar = () => {
-  const { setModalsState } = useContext(ModalContext);
-
-  const [service, setService] = useState('');
-
   const theme = useTheme();
 
-  const handleClick = <Keys extends ModalsStateKeys>(key: Keys) => {
+  const { setModalsState } = useContext(ModalContext);
+
+  const { services } = useContext(ServicesContext);
+
+  const { service, setService } = useContext(ServiceContext);
+
+  const { setOccurrences } = useContext(OccurrencesContext);
+
+  const { setIsEditing } = useContext(EditingContext);
+
+  useEffect(() => {
+    if (service.serviceName !== '') {
+      setIsEditing(true);
+    }
+  }, [service.serviceName]);
+
+  const handleClick = async <Keys extends ModalsStateKeys>(key: Keys) => {
     setModalsState((prevValue) => ({
       ...prevValue,
       [key]: true,
     }));
   };
 
-  const handleServiceSelectChange = (event: SelectChangeEvent<unknown>) => {
+  const handleServiceSelectChange = async (
+    event: SelectChangeEvent<unknown>
+  ) => {
     const selectedValue = event.target.value as string;
     if (selectedValue === 'new-service') {
-      setService(selectedValue);
       handleClick('isServiceModalOpen');
+      await setService({ serviceName: '', _id: '' });
+      setIsEditing(false);
     }
-    setService('');
   };
+
+  const handleOccurrences = async (serviceName: string, _id: string) => {
+    await setService({ serviceName, _id });
+    const occurrences = await getOccurrences(_id);
+    setOccurrences(occurrences);
+  };
+
+  const toggleButton = () => (service.serviceName === '' ? true : false);
 
   return (
     <S.OutterBox theme={theme}>
@@ -41,18 +68,27 @@ const ModalToolBar = () => {
           <S.ServiceSelect
             displayEmpty
             inputProps={{ 'aria-label': 'Without label' }}
-            value={service}
+            value={service.serviceName}
             onChange={handleServiceSelectChange}
           >
-            <MenuItem value="new-service">Novo Serviço</MenuItem>
+            <MenuItem value={'new-service'}>Novo Serviço</MenuItem>
+            {services.map(({ serviceName, _id }, index) => (
+              <MenuItem
+                value={serviceName}
+                key={index}
+                onClick={() => handleOccurrences(serviceName, _id)}
+              >
+                {serviceName}
+              </MenuItem>
+            ))}
           </S.ServiceSelect>
         </S.ServiceFormControl>
         <Typography>
           <b>Data inicial:</b> 18/10/2022
         </Typography>
         <S.ButtonContainer theme={theme}>
-          <EditButton modal="isSessionModalOpen" />
-          <DeleteButton />
+          <EditButton modal="isServiceModalOpen" isDisabled={toggleButton()} />
+          <DeleteButton isDisabled={toggleButton()} />
         </S.ButtonContainer>
       </S.InnerBox>
       <S.ToolBarDivider theme={theme} />
@@ -62,6 +98,7 @@ const ModalToolBar = () => {
             variant="text"
             startIcon={<SessionIcon filled={true} />}
             onClick={() => handleClick('isSessionModalOpen')}
+            disabled={toggleButton()}
           >
             <S.ActionButtonTypography color="secondary.dark">
               Sessão
@@ -71,6 +108,7 @@ const ModalToolBar = () => {
             variant="text"
             startIcon={<PinIcon filled={true} />}
             onClick={() => handleClick('isPertinentFactModalOpen')}
+            disabled={toggleButton()}
           >
             <S.ActionButtonTypography color="secondary.dark">
               Fato importante
@@ -80,6 +118,7 @@ const ModalToolBar = () => {
             variant="text"
             startIcon={<ClipIcon filled={true} />}
             onClick={() => handleClick('isAttachmentModalOpen')}
+            disabled={toggleButton()}
           >
             <S.ActionButtonTypography color="secondary.dark">
               Anexo
@@ -91,6 +130,7 @@ const ModalToolBar = () => {
               <ClipboardIcon color="#EA1E61" width="24" height="29.33" />
             }
             onClick={() => handleClick('isPsychologicalAssessmentModalOpen')}
+            disabled={toggleButton()}
           >
             <S.ActionButtonTypography color="secondary.dark">
               Avaliação Psicológica
