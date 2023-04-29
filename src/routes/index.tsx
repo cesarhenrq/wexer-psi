a00228d1563cef0ddc7a6a7a3fdb8d9934448bd4
cesarhenrq <cesaarhvb@gmail.com>
2023-04-29 19:07:36 -0300

    refactor: change routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import TabNav from '../components/tab-nav';
+import RecordPage from '../pages/record-page';
 import BaseLayout from '../pages/base-layout';
-import MedicalRecord from '../pages/medical-record';
-import RegistrationData from '../pages/registration-data';
-import PsychologicalAssessment from '../components/psychological-assessment';
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route element={<BaseLayout />}>
-        <Route element={<TabNav />}>
-          <Route index element={<RegistrationData />} />
-          <Route path="dados-cadastrais" element={<RegistrationData />} />
-          <Route path="prontuário" element={<MedicalRecord />} />
-        </Route>
-        <Route
-          path="entrevista-psicologica"
-          element={<PsychologicalAssessment />}
-        />
+      <Route path="" element={<BaseLayout />}>
+        <Route path="prontuário" element={<RecordPage />} />
       </Route>
     </Routes>
   </BrowserRouter>
