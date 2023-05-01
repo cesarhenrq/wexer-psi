e0c812e081009caec2ad68328a83eff5429c4de0
cesarhenrq <cesaarhvb@gmail.com>
2023-05-01 01:56:18 -0300

    feat: add api integration

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RecordPage from '../pages/record-page';
 import BaseLayout from '../pages/base-layout';
+import SessionPage from '../pages/session-page';
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="" element={<BaseLayout />}>
-        <Route path="prontuário" element={<RecordPage />} />
+      <Route path="/" element={<BaseLayout />}>
+        <Route path="prontuário" element={<RecordPage />} index />
+        <Route path="sessão" element={<SessionPage />} />
       </Route>
     </Routes>
   </BrowserRouter>
